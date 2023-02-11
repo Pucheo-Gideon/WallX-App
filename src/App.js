d063fe1b6bfc35c1aa68f60c46aae33368faa9c3
Pucheo-Gideon <okwongudogideon123@gmail.com>
2023-02-11 05:29:47 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Box, Container } from "@mui/material";
+import WallXPage from "./WallXApp2/wallx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WallXPage />
+    // <Container maxWidth={false} sx={{p:0}} disableGutters>
+
+    // </Container>
+    // <Box sx={{width: "1728px",height: "5448px", position:"relative", background: "FFFFFF"}}>
+    // </Box>
   );
 }
 
